68b789d0293f97d748ec919c5819b148e80a257c
PC Admin Local <pcvhit@LT-DE2302082.japan.gds.panasonic.com>
2025-04-09 12:05:20 +0700

    09042025-1

---
+++ Day5/getdatatraning.py
@@ -0,0 +1,31 @@
+import cv2
+import datetime
+
+video = cv2.VideoCapture(1)
+while(True):
+
+    Ret,Frame = video.read()
+    if(Ret):
+        cv2.imshow("img",Frame)
+    
+    waitkey = cv2.waitKey(1)
+
+    if(waitkey == ord('f')):
+        filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        print(filename)
+        
+        rs = cv2.imwrite(r"C:\Users\Administrator\Desktop\StudyAI\Day5\faces\\"+filename+".jpg",Frame)
+        print(rs)
+        
+            
+    if(waitkey == ord('n')):
+        filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        print(filename)
+        rs = cv2.imwrite(r"C:\Users\Administrator\Desktop\StudyAI\Day5\non_faces\\"+filename+".jpg",Frame)
+        print(rs)
+
+    if(waitkey == ord('q')):
+        break
+
+video.release()
+cv2.destroyAllWindows()
